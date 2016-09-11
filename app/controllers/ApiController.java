c62986ce75ad4469cbb275cf56fdcdc4d2a4726d
Masaya Uno <uno@nlp.ist.i.kyoto-u.ac.jp>
2016-09-12 02:15:07 +0900

    controllers/ApiController: show,create,delete,update を実装

--- app/controllers/ApiController.java
+++ app/controllers/ApiController.java
@@ -8,6 +8,7 @@ import com.avaje.ebean.annotation.Transactional;
 import com.fasterxml.jackson.databind.node.ObjectNode;
 
 import play.libs.Json;
+import com.fasterxml.jackson.databind.JsonNode;
 import play.mvc.Controller;
 import play.mvc.Result;
 import models.Item;
@@ -28,10 +29,80 @@ public class ApiController extends Controller{
 	@Transactional
 	public Result showAll() {
 		// 全itemを取得して、jsonで返す
+		// いくつitemがあっても全て返す(開発用) 返すitem数を制限したAPIも後で作る
 		List<Item> items = Item.finder.all();
 
 		ObjectNode result = Json.newObject();
 		result.put("items", Json.toJson(items));
+		return ok(result);
+	}
+	
+	public Result showItems() {
+		// 全itemを取得して、jsonで返す
+		// 返すitem数を制限し(10-100の間, クエリパラメータで指定)、
+		// 何ページ目かを指定
+		
+		// とりあえずshowAllしておく
+		return showAll();
+	}
+	
+	@Transactional
+	public Result show(Long id) {
+		Item item = Item.finder.byId(id);
+		if(item == null) {
+			return badRequest("Item id " + id + " not found.");
+		}
+		return ok(Json.toJson(item));
+	}
+	
+	@Transactional
+	public Result createItem() {
+		JsonNode json = request().body().asJson();
+		if(json == null) {
+			return badRequest("Expecting Json data");
+		}
+		Item item = Item.fromJson(json);
+		if(Item.finder.byId(item.id) != null) {
+			return badRequest("Item id " + item.id + " already exists.");
+		}
+		item.save();
+
+		ObjectNode result = Json.newObject();
+		result.put("message", "Item saved.");
+		result.put("item", Json.toJson(item));
+		return ok(result);
+	}
+	
+	@Transactional
+	public Result deleteItem(Long id) {
+		Item item = Item.finder.byId(id);
+		item.delete();
+		ObjectNode result = Json.newObject();
+		result.put("message", "Item deleted.");
+		result.put("item", Json.toJson(item));
+		return ok(result);
+	}
+	
+	@Transactional
+	public Result updateItem(Long id) {
+		JsonNode json = request().body().asJson();
+		if(json == null) {
+			return badRequest("Expecting Json data");
+		}
+		Item oldItem = Item.finder.byId(id);
+		if(oldItem == null) {
+			return badRequest("Item id " + id + " not found.");
+		}
+		
+		Item newItem = Item.fromJson(json);
+		if(newItem.id != oldItem.id) {
+			return badRequest("Item ID not equals.");
+		}
+		newItem.update();
+
+		ObjectNode result = Json.newObject();
+		result.put("message", "Item updated.");
+		result.put("item", Json.toJson(newItem));
 		return ok(result);
 	}
 }
--- app/models/Item.java
+++ app/models/Item.java
@@ -8,8 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.validation.constraints.Size;
 import javax.validation.constraints.NotNull;
 import com.avaje.ebean.annotation.CreatedTimestamp;
-
+import com.fasterxml.jackson.databind.JsonNode;
 import com.avaje.ebean.Model;
+
+// import com.fasterxml.jackson.databind.JsonNode;
 
 @Entity(name = "item")
 public class Item extends Model{
@@ -44,4 +46,33 @@ public class Item extends Model{
 	
 	public static final Find<Long, Item> finder = new Find<Long, Item>(){};
 	
+	
+	public static Item fromJson(JsonNode json) {
+		Long id = null;
+		String title = null;
+		String description = null;
+		Long price = null;
+		String picture_uri = null;
+		if(json.has("id")) {
+			id = json.findPath("id").asLong();
+		} else {
+			// raise;
+		}
+		if(json.has("title")) {
+			title = json.findPath("title").textValue();
+		} else {
+			// raise;
+		}
+		if(json.has("description")) {
+			description = json.findPath("description").textValue();
+		}
+		if(json.has("price")) {
+			price = json.findPath("price").asLong();
+		}
+		if(json.has("picture_uri")) {
+			picture_uri = json.findPath("picture_uri").textValue();
+		}
+		
+		return new Item(id, title, description, price, picture_uri);
+	}
 }
